aef7573d3c8795afdecdcf7f88d902b0cd867967
carolinilaila <carolinilaila@hotmail.com>
2022-08-30 13:38:56 -0300

    feat: creating search engine

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,12 @@
+import './Search.css'
+
+function Search() {
+  return (
+    <div className="b-search__content">
+      <input placeholder="search for game" type="text"></input>
+      <button type="button">Search</button>
+    </div>
+  )
+}
+
+export default Search
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,11 @@
 import Header from '../components/Header/Header'
+import Search from '../components/Search/Search'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <p>Home</p>
+      <Search />
     </div>
   )
 }
